aa8826b7005c5aa9e5475f663d12ab36ed48a4f4
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2023-12-18 12:17:36 +1300

    Added boxshadows.

--- freshfinds/src/components/Home/ButtonBar.jsx
+++ freshfinds/src/components/Home/ButtonBar.jsx
@@ -2,7 +2,7 @@ const ButtonBar = () => {
   return (
     <div className="space-x-3 flex">
       <button
-        className="text-white px-4 py-2 text-center rounded-sm"
+        className="text-white px-4 py-2 text-center rounded-sm shadow-md"
         style={{
           backgroundColor: "#3D3D3D",
         }}
@@ -10,7 +10,7 @@ const ButtonBar = () => {
         Sign Up
       </button>
       <button
-        className="text-white px-6 py-2 text-center rounded-sm"
+        className="text-white px-6 py-2 text-center rounded-sm shadow-md"
         style={{
           backgroundColor: "#FF542F",
         }}
